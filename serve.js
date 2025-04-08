bb15287c84bffdbdee534696bd29a309595949c8
Daniel Alves <danielallves903@gmail.com>
2025-04-08 08:19:33 -0300

    adicionado banco

--- serve.js
+++ serve.js
@@ -1,66 +1,53 @@
-import express from "express";
-import path from "path";
-import session from "express-session";
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+const express = require("express");
+const path = require("path");
+const bcrypt = require("bcrypt");
+const ejs = require("ejs");
+const db = require("./banco");
 
 const app = express();
+const basepath = path.join(__dirname);
 
-const usuarios = [];
-
-app.use(express.static(path.join(__dirname, "public")));
-app.use(session({ secret: "opa", saveUninitialized: true, resave: false }));
-app.use(express.urlencoded({ extended: true }));
-
+app.use(express.json());
+app.use(express.static(path.join(basepath, "public")));
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(basepath, "views"));
 
-app.get("/", (req, res) => {
-  if (req.session.usuario) {
-    res.render("inicial", { usuario: req.session.usuario });
-  } else {
-    res.render("cadastro");
-  } 
+app.get("/cadastro", (req, res) => {
+  res.render("cadastro");
 });
 
-app.get("/login", (req, res) => {
-  if (req.session.usuario) {
-    res.render("inicial", { usuario: req.session.usuario });
-  } else {
-    res.render("login");
-  }
-});
+app.post("/cadastro", (req, res) => {
+  const { usuario, senha } = req.body;
+  let nivel_seguranca = 10;
 
-app.post("/", (req, res) => {
-  const { nome, senha } = req.body;
-  usuarios.push({ nome, senha });
-  console.log(usuarios);
-  res.redirect("login");
-});
+  console.log(`usuario: ${usuario}, senha: ${senha}`)
 
-app.post("/login", (req, res) => {
-  const { usulog, senhalog } = req.body;
+  const verificausuario = `SELECT * FROM usuarios WHERE usuario = ?`;
 
-  const usuariosencontrados = usuarios.find(
-    (user) => user.nome === usulog && user.senha === senhalog
-  );
-
-  if (usuariosencontrados) {
-    req.session.usuario = usuariosencontrados;
-    res.render("inicial", { usuario: usuariosencontrados });
-  } else {
-    res.render("login");
-  }
-});
-
-app.get("/sair", (req, res) => {
-  req.session.destroy((erro) => {
-    if (erro) {
-      res.redirect("inicil");
-    } else {
-      res.redirect("login");
+  db.all(verificausuario, [usuario], (err, rows) => {
+    if (err) {
+      return res.status(500).json({ msg: "Erro ao selecionar usuario" });
     }
+
+    if (rows.length > 0) {
+      return res.status(409).json({ msg: "O usuario ja existe" });
+    }
+
+    bcrypt.hash(senha, nivel_seguranca, (err, hash) => {
+      if (err) {
+        return res.status(500).json({ msg: "Erro ao criptografar senha" });
+      }
+
+      const query = `INSERT INTO usuarios (usuario, senha) VALUES (?, ?)`;
+
+      db.run(query, [usuario, hash], function (err) {
+        if (err) {
+          return res.status(500).json({ msg: "Erro ao inserir usuario" });
+        }
+
+        res.status(200).json({ msg: "Usuário cadastrado", id: this.lastID });
+      });
+    });
   });
 });
 
